c7d3cbe4a35d92479809de8fc03d69aa9350678f
jaedle <dennis.jekubczyk@gmail.com>
2019-02-15 21:30:00 +0100

    add tests

--- internal/hs100connector/crypto_test.go
+++ internal/hs100connector/crypto_test.go
@@ -14,21 +14,21 @@ var _ = Describe("Hs100crypto", func() {
 	Describe("encrypt / decrypt", func() {
 		It("encrypts and decrypts", func() {
 			plain := "{command: example}"
-			e := hs100connector.Encrypt(plain)
-			d := hs100connector.Decrypt(e)
-
-			Expect(plain).To(Equal(d))
+			result := hs100connector.Decrypt(hs100connector.Encrypt(plain))
+			Expect(result).To(Equal(plain))
 		})
 
-		It("encrypts fixtures", func() {
-			e := hs100connector.Encrypt(`{"system":{"set_relay_state":{"state":1}}}`)
-			expected, _ := b64.StdEncoding.DecodeString("0PKB+Iv/mvfV75S2xaDUi/mc8JHot8Sw0aXA4tijgfKG55P21O7fot+i")
-			Expect(e).To(Equal(expected))
-		})
+		DescribeTable("encrypts fixtures", func(plain string, encrypted string) {
+			expected, _ := b64.StdEncoding.DecodeString(encrypted)
+			Expect(hs100connector.Encrypt(plain)).To(Equal(expected))
+		},
+			Entry("fixture-1", `{"system":{"set_relay_state":{"state":1}}}`, "0PKB+Iv/mvfV75S2xaDUi/mc8JHot8Sw0aXA4tijgfKG55P21O7fot+i"),
+			Entry("fixture-2", `{ "system":{ "get_sysinfo":null } }`, "0PDSodir37rX9c+0lLbRtMCf7JXmj+GH6MrwnuuH68u2lus="),
+		)
 
-		DescribeTable("decrypts fixtures", func(encrypted string, decrypted string) {
+		DescribeTable("decrypts fixtures", func(encrypted string, plain string) {
 			e, _ := b64.StdEncoding.DecodeString(encrypted)
-			Expect(hs100connector.Decrypt(e)).To(Equal(decrypted))
+			Expect(hs100connector.Decrypt(e)).To(Equal(plain))
 		},
 			Entry("fixture-1", "0PKB+Iv/mvfV75S2xaDUi/mc8JHot8Sw0aXA4tijgfKG55P21O7fot+i", `{"system":{"set_relay_state":{"state":1}}}`),
 			Entry("fixture-2", "0PDSodir37rX9c+0lLbRtMCf7JXmj+GH6MrwnuuH68u2lus=", `{ "system":{ "get_sysinfo":null } }`),
