b6e394483461430c7fdbd1fa686d876c76dcd1ec
jaedle <dennis.jekubczyk@gmail.com>
2019-02-15 15:19:15 +0100

    refactoring

--- internal/hs100connector/connector_test.go
+++ internal/hs100connector/connector_test.go
@@ -1,7 +1,6 @@
 package hs100connector_test
 
 import (
-	"log"
 	"net"
 	"time"
 
@@ -11,15 +10,15 @@ import (
 )
 
 var _ = Describe("Connector", func() {
-	It("returns no error", func() {
+	It("sends expected command", func() {
 		l, err := net.Listen("tcp", ":9999")
 		if err != nil {
-			log.Fatal(err)
+			Fail("could not start test server")
 		}
 		defer l.Close()
 
 		requestContent := make(chan string)
-		go startServer(l, requestContent)
+		go handleRequest(l, requestContent)
 
 		err2 := hs100connector.SendCommand(aDevice("127.0.0.1"))
 
@@ -37,20 +36,20 @@ var _ = Describe("Connector", func() {
 
 })
 
-func startServer(l net.Listener, response chan string) {
+func handleRequest(l net.Listener, response chan string) {
 	conn, err := l.Accept()
 	if err != nil {
 		Fail("can not start server")
 	}
 
 	buf := make([]byte, 1024)
-	n, err2 := conn.Read(buf)
-	if err2 != nil {
+	n, err := conn.Read(buf)
+	if err != nil {
 		Fail("can not read request")
 	}
 	received := string(buf[:n])
 	print(received)
-	_, _ = conn.Write([]byte("Message received."))
+	_, _ = conn.Write([]byte(""))
 	_ = conn.Close()
 	response <- received
 }
