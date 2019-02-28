2351cf96b6960a1a12772a7265d7a580ab147d54
jaedle <dennis.jekubczyk@gmail.com>
2019-02-28 15:58:29 +0100

    add draft for integration test

--- test/integration/integration_test.go
+++ test/integration/integration_test.go
@@ -1,12 +1,22 @@
 package integration_test
 
 import (
+	"github.com/jaedle/golang-tplink-hs100/pkg/configuration"
+	"github.com/jaedle/golang-tplink-hs100/pkg/hs100"
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
 )
 
 var _ = Describe("Integration", func() {
 	It("fails", func() {
-		Expect(true).To(Equal(true))
+		h := hs100.NewHs100("localhost", configuration.Default())
+		isOn, err := h.IsOn()
+		Expect(err).NotTo(HaveOccurred())
+		Expect(isOn).To(BeFalse())
+
+		h.TurnOn()
+		isOn, err = h.IsOn()
+		Expect(err).NotTo(HaveOccurred())
+		Expect(isOn).To(BeTrue())
 	})
 })
