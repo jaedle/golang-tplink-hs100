ca15e82c52cef4b2c8fc2a334f4526c8a94358d7
jaedle <dennis.jekubczyk@gmail.com>
2020-02-01 14:27:04 +0100

    extract discovery

--- pkg/hs100/hs100.go
+++ pkg/hs100/hs100.go
@@ -3,8 +3,6 @@ package hs100
 import (
 	"encoding/json"
 	"github.com/pkg/errors"
-	"net"
-	"sync"
 )
 
 const turnOnCommand = `{"system":{"set_relay_state":{"state":1}}}`
@@ -169,68 +167,5 @@ func (r *powerConsumptionResponse) toPowerConsumption() PowerConsumption {
 		Current: r.Emeter.RealTime.Current,
 		Voltage: r.Emeter.RealTime.Voltage,
 		Power:   r.Emeter.RealTime.Power,
-	}
-}
-
-func Discover(subnet string, s CommandSender) ([]*Hs100, error) {
-	ips, err := getIpAddresses(subnet)
-	if err != nil {
-		return nil, err
-	}
-
-	result := &discoverResult{
-		devices: make([]*Hs100, 0),
-		Mutex:   sync.Mutex{},
-	}
-	var wg sync.WaitGroup
-
-	wg.Add(len(ips))
-	for _, current := range ips {
-		go func(r *discoverResult, w *sync.WaitGroup, ip string) {
-			defer w.Done()
-
-			hs100 := NewHs100(ip, s)
-			_, err := hs100.GetName()
-			if err != nil {
-				return
-			}
-
-			r.Lock()
-			defer r.Unlock()
-			r.devices = append(result.devices, hs100)
-		}(result, &wg, current)
-	}
-
-	wg.Wait()
-
-	return result.devices, nil
-}
-
-type discoverResult struct {
-	devices []*Hs100
-	sync.Mutex
-}
-
-func getIpAddresses(subnet string) ([]string, error) {
-	ip, ipnet, err := net.ParseCIDR(subnet)
-	if err != nil {
-		return nil, errors.Wrap(err, "invalid subnet specfied")
-	}
-
-	var ips []string
-	for ip := ip.Mask(ipnet.Mask); ipnet.Contains(ip); inc(ip) {
-		ips = append(ips, ip.String())
-	}
-
-	return ips[1 : len(ips)-1], nil
-}
-
-//  http://play.golang.org/p/m8TNTtygK0
-func inc(ip net.IP) {
-	for j := len(ip) - 1; j > 0; j-- {
-		ip[j]++
-		if ip[j] > 0 {
-			break
-		}
 	}
 }
